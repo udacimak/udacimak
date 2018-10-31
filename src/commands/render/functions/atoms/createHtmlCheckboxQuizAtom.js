af7eb9335243281a7c9a21d70cebfd8607613fd8
udacimak <udacimak@tutanota.com>
2018-11-01 08:53:23 +1100

    Fix radio and checkbox quiz to show solution
    The solution is included in json data. Add this to the HTML output.

--- src/commands/render/functions/atoms/createHtmlCheckboxQuizAtom.js
+++ src/commands/render/functions/atoms/createHtmlCheckboxQuizAtom.js
@@ -12,8 +12,15 @@ export default function createHtmlCheckboxQuizAtom(atom) {
   const prompt = markdownToHtml(atom.question.prompt);
 
   let answers = [];
+  let solution = [];
   for (const answer of atom.question.answers) {
-    const { id, text } = answer;
+    const { id, text, is_correct } = answer;
+
+    // if this is the correct answer, add to solution
+    if ('is_correct' in answer && is_correct !== null && is_correct === true) {
+      solution.push(answer);
+    }
+
     answers.push({
       id,
       name: atom.id,
@@ -25,6 +32,7 @@ export default function createHtmlCheckboxQuizAtom(atom) {
     .then(html => {
       const dataTemplate = {
         answers,
+        solution,
         prompt
       };
       const template = Handlebars.compile(html);
--- src/commands/render/functions/atoms/createHtmlRadioQuizAtom.js
+++ src/commands/render/functions/atoms/createHtmlRadioQuizAtom.js
@@ -12,13 +12,20 @@ export default function createHtmlRadioQuizAtom(atom) {
   const prompt = markdownToHtml(atom.question.prompt);
 
   let answers = [];
+  let solution;
   for (const answer of atom.question.answers) {
-    let { id, text } = answer;
+    let { id, text, is_correct } = answer;
+    text = markdownToHtml(text);
+
+    // if this is the correct answer, add to solution
+    if ('is_correct' in answer && is_correct !== null && is_correct === true) {
+      solution = answer;
+    }
 
     answers.push({
       id,
       name: atom.id,
-      text: markdownToHtml(text)
+      text
     });
   }
 
@@ -26,6 +33,7 @@ export default function createHtmlRadioQuizAtom(atom) {
     .then(html => {
       const dataTemplate = {
         answers,
+        solution,
         prompt
       };
       const template = Handlebars.compile(html);
