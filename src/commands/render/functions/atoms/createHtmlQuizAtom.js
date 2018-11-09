383c83e1c6c399478cb65b1452e338dbf54150dc
udacimak <udacimak@tutanota.com>
2018-11-09 22:32:22 +1100

    Fix download promise order in HtmlQuizAtom

--- src/commands/render/functions/atoms/createHtmlQuizAtom.js
+++ src/commands/render/functions/atoms/createHtmlQuizAtom.js
@@ -18,57 +18,61 @@ import {
  * @returns {string} HTML content
  */
 export default async function createHtmlQuizAtom(atom, targetDir, prefix) {
-  const { question } = atom;
-  const semanticType = question.semantic_type;
-  let promiseQuizQuestion;
+  try {
+    const { question } = atom;
+    const semanticType = question.semantic_type;
+    let promiseQuizQuestion;
 
-  // process different semantic types of QuizAtom
-  if (semanticType === 'ProgrammingQuestion' || semanticType === 'IFrameQuestion') {
-    promiseQuizQuestion = createHtmlQuizProgrammingQuestion(atom);
-  } else if (semanticType === 'ImageFormQuestion') {
-    promiseQuizQuestion = createHtmlQuizImageFormQuestion(atom, targetDir, prefix);
-  } else {
-    const msg = 'Unknown quiz type. Please contact the developer to make it compatible with this atom type!';
-    promiseQuizQuestion = new Promise(resolve => resolve(msg));
+    // download instruction video if available
+    const youtubeIdQuestion = (atom.instruction && atom.instruction.video)
+      ? atom.instruction.video.youtube_id : '';
+    const filenameYoutubeQuestion = await downloadYoutube(youtubeIdQuestion,
+      targetDir, prefix, atom.title);
+
+    // process different semantic types of QuizAtom
+    if (semanticType === 'ProgrammingQuestion' || semanticType === 'IFrameQuestion') {
+      promiseQuizQuestion = createHtmlQuizProgrammingQuestion(atom);
+    } else if (semanticType === 'ImageFormQuestion') {
+      promiseQuizQuestion = createHtmlQuizImageFormQuestion(atom, targetDir, prefix);
+    } else {
+      const msg = 'Unknown quiz type. Please contact the developer to make it compatible with this atom type!';
+      promiseQuizQuestion = new Promise(resolve => resolve(msg));
+    }
+
+    const htmlQuiz = await promiseQuizQuestion;
+
+    // all other promises
+    const youtubeIdAnswer = (atom.answer && atom.answer.video)
+      ? atom.answer.video.youtube_id : '';
+    const promiseDownloadYoutubeAnswer = downloadYoutube(youtubeIdAnswer,
+      targetDir, prefix, atom.title);
+    const promiseLoadTemplate = loadTemplate('atom.quiz');
+
+    const [
+      filenameYoutubeAnswer,
+      html,
+    ] = await Promise.all([
+      promiseDownloadYoutubeAnswer,
+      promiseLoadTemplate,
+    ]);
+
+    const instruction = atom.instruction ? markdownToHtml(atom.instruction.text) : '';
+    const answerText = atom.answer ? markdownToHtml(atom.answer.text) : '';
+    const hasSolution = filenameYoutubeAnswer || answerText;
+    const hasInstruction = filenameYoutubeQuestion || instruction;
+
+    const dataTemplate = {
+      answerText,
+      instruction,
+      filenameYoutubeQuestion,
+      filenameYoutubeAnswer,
+      hasSolution,
+      hasInstruction,
+      htmlQuiz,
+    };
+    const template = Handlebars.compile(html);
+    return template(dataTemplate);
+  } catch (error) {
+    throw error;
   }
-
-  // download instruction video if available
-  const youtubeIdQuestion = (atom.instruction && atom.instruction.video)
-    ? atom.instruction.video.youtube_id : '';
-  const filenameYoutubeQuestion = await downloadYoutube(youtubeIdQuestion,
-    targetDir, prefix, atom.title);
-
-  // all other promises
-  const youtubeIdAnswer = (atom.answer && atom.answer.video)
-    ? atom.answer.video.youtube_id : '';
-  const promiseDownloadYoutubeAnswer = downloadYoutube(youtubeIdAnswer,
-    targetDir, prefix, atom.title);
-  const promiseLoadTemplate = loadTemplate('atom.quiz');
-
-  const [
-    filenameYoutubeAnswer,
-    html,
-    htmlQuiz,
-  ] = await Promise.all([
-    promiseDownloadYoutubeAnswer,
-    promiseLoadTemplate,
-    promiseQuizQuestion,
-  ]);
-
-  const instruction = atom.instruction ? markdownToHtml(atom.instruction.text) : '';
-  const answerText = atom.answer ? markdownToHtml(atom.answer.text) : '';
-  const hasSolution = filenameYoutubeAnswer || answerText;
-  const hasInstruction = filenameYoutubeQuestion || instruction;
-
-  const dataTemplate = {
-    answerText,
-    instruction,
-    filenameYoutubeQuestion,
-    filenameYoutubeAnswer,
-    hasSolution,
-    hasInstruction,
-    htmlQuiz,
-  };
-  const template = Handlebars.compile(html);
-  return template(dataTemplate);
 }
