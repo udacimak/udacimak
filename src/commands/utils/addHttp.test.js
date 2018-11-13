129a9cd3c36390734a562afa044e6cba1139f315
udacimak <udacimak@tutanota.com>
2018-11-13 23:20:51 +1100

    Add test setup

---
+++ src/commands/utils/addHttp.test.js
@@ -0,0 +1,22 @@
+import addHttp from './addHttp';
+
+
+test('Do nothing to correct url', () => {
+  const url = 'https://www.google.com';
+  expect(addHttp(url)).toBe(url);
+});
+
+test('Fix wrong urls', () => {
+  const urls = [
+    'www.google.com',
+    '//www.google.com',
+    'http:///www.google.com',
+    'https:////www.google.com',
+  ];
+  let correctUrl = 'https://www.google.com';
+
+  for (let i = 0, len = urls.length; i < len; i += 1) {
+    expect(addHttp(urls[i])).toBe(correctUrl);
+  }
+});
+
---
+++ src/commands/utils/getFileExt.test.js
@@ -0,0 +1,16 @@
+import getFileExt from './getFileExt';
+
+
+test('Returns correct extension', () => {
+  const filenames = [
+    '01. test.en-us.mp4',
+    'test.en-us',
+    'test.en-us.mp4',
+  ];
+
+  const correct = 'en-us.mp4';
+
+  for (let i = 0, len = filenames.length; i < len; i += 1) {
+    expect(getFileExt(filenames[i])).toBe(correct);
+  }
+});
